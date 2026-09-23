0dd06e2e5a83c64269c6bd7d30350f2e58abc6b2
Melinn <chailomsupitcha@gmail.com>
2026-09-23 14:53:52 -0400

    test if-else function to generate 0,1 and decides which colour each 0 or 1 gets.

--- instructions-prototype/abstract-prototype/js/script.js
+++ instructions-prototype/abstract-prototype/js/script.js
@@ -21,10 +21,36 @@ function setup() {
   noLoop(); //
 }
 
-
 /**
- * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
-*/
+ * Binary
+ */
 function draw() {
+  // Light grey background
+  background(225);
 
+  for (let y = cellSize / 2; y < height; y += cellSize) {
+    for (let x = cellSize / 2; x < width; x += cellSize) {
+      let bit = floor(random(2)); // 0 or 1
+      let lensDistance = dist(x, y, 300, 225);
+
+      // Lens
+      if (dist(x, y, 275, 200) < 14) {
+        fill(255, 245, 230);
+      }
+      // Lens centre
+      else if (lensDistance < 50) {
+        fill(50, 25, 10);
+      }
+      // Lens ring
+      else if (lensDistance < 72) {
+        fill(215, 170, 120);
+      }
+      // Lens edge
+      else if (lensDistance < 84) {
+        fill(70, 40, 20);
+
+      text(bit, x, y);
+      }
+    }
+  }
 }
