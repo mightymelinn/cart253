c71de25cdbf0181ef264d7afd26f24667429907d
Melinn <chailomsupitcha@gmail.com>
2026-09-23 15:34:30 -0400

    fixed bugs; close comment + added } to bring mousePressed outside draw function

--- instructions-prototype/weird-prototype/js/script.js
+++ instructions-prototype/weird-prototype/js/script.js
@@ -3,6 +3,7 @@
  * Author Name: Melinn (Supitcha Chailom)
  * 
  * This is my attempt to try ripple fx. I found this tutorial on YouTube and I thought it was really cool.
+ */
 
 "use strict";
 
@@ -10,19 +11,39 @@
 let rippleX = 0;
 let rippleY = 0;
 let rippleSize = 0;
-let rippleColour;
+let rippleHue = 0;
 
 /**
- * OH LOOK I DIDN'T DESCRIBE SETUP!!
+ * Set up Canvas
 */
 function setup() {
-
+  createCanvas(600, 400);
+  colorMode(HSB, 360, 100, 100);
+  noFill();
+  strokeWeight(6);
 }
 
 
 /**
- * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
+ * Draws the ripple growing bigger every frame.
 */
 function draw() {
+  background(330, 8, 100);
 
+  // Only draw the ripple after a click
+  if (rippleSize > 0) {
+    stroke(rippleHue, 80, 100);
+    ellipse(rippleX, rippleY, rippleSize);
+    rippleSize += 8;
+  }
+}
+
+/**
+ * Adding MousePressed function to allow ripple drawing only when the mouse is clicked
+*/
+function mousePressed() {
+  rippleX = mouseX;
+  rippleY = mouseY;
+  rippleSize = 1;
+  rippleHue = random(360);
 }
